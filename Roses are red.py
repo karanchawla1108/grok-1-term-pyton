38621402141736c8f701aa20df2c4042f7b65905
karanchawla1108 <karanchawla1108@gmail.com>
2024-07-04 19:05:14 +0100

    Roses are red

---
+++ Roses are red.py
@@ -0,0 +1,33 @@
+# You're writing some new versions of the poem Roses are Red.
+
+# 1.
+# Click Run to see what you have so far.
+
+# 2.
+# Change the line1 and line2 variables to try out some new lines:
+
+# The first line should say 'Fire trucks are red'
+
+# The second line should say 'Police cars are blue'
+
+# 3.
+# Run your program again. It should print the following:
+
+
+
+
+
+line1 = 'Fire trucks are red'
+line2 = 'Police cars are blue'
+
+print(line1)
+print(line2)
+print('Sugar is sweet')
+print('And so are you')
+print()
+
+print(line1)
+print(line2)
+print('Most poems rhyme')
+print('But not this one')
+
