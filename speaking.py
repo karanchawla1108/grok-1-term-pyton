38c9f07dca4c523a1171b5c22da05b985c2e5db8
karanchawla1108 <karanchawla1108@gmail.com>
2024-07-04 21:28:02 +0100

    Speaking Backwards

---
+++ speaking.py
@@ -0,0 +1,17 @@
+# Write a program that reads in a line of text and prints out the line of text backwards.
+
+# Line: This is an example line of text.
+# .txet fo enil elpmaxe na si sihT
+# ​
+# Here's another example:
+
+# Line: HeLLo ThErE!
+#ErEhT oLLeH
+# ​
+
+
+line = input("Line: ")
+
+reverse = line[::-1]
+
+print (reverse)
