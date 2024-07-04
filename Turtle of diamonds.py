75452eb77231be6ea5e404a6e0bbc993b85fbab7
karanchawla1108 <karanchawla1108@gmail.com>
2024-07-04 20:22:46 +0100

    Diamonds

---
+++ Turtle of diamonds.py
@@ -0,0 +1,54 @@
+# Let's draw the Two of Diamonds playing card, like this:
+
+  
+# 1.
+# Set the pencolor to 'red' and the pensize to 5.
+
+# 2.
+# Get into position to draw the first diamond by turning 45 degrees left.
+
+# 3.
+# Draw the first diamond! Move 30 steps, then turn 90 degrees left.
+
+# Move forward three more times to complete the diamond. Make sure to turn between each side!
+
+# 4.
+# Get into place for the second diamond. Lift the pen, then turn right 45 degrees.
+
+# Click run and make sure the turtle is facing straight down.
+
+# Move forward 40 steps, then turn 45 degrees right again. Don't forget to put the pen back down!
+
+# 5.
+# Draw the second diamond to match the first.
+
+# 6.
+# Run your code, check if it looks right, then mark it!
+
+
+from turtle import *
+pensize(5)
+pencolor('red')
+left(45)
+forward(30)
+left(90)
+forward(30)
+left(90)
+forward(30)
+left(90)
+forward(30)
+penup()
+right(45)
+forward(40)
+
+right(45)
+pendown()
+forward(30)
+left(90)
+forward(30)
+left(90)
+forward(30)
+left(90)
+forward(30)
+
+
