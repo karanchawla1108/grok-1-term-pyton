14b34bcf4ad0ea8430d33dbb9a08ff8b2a64f5be
karanchawla1108 <karanchawla1108@gmail.com>
2024-07-04 19:16:59 +0100

    munda

---
+++ munda biddi trail.py
@@ -0,0 +1,33 @@
+# You're cycling the Munda Biddi Trail.
+
+# Let's work out how far you still need to go! We'll use these numbers:
+
+# The trail is 1000 kilometres long;
+# You cycle 70 kilometres a day;
+# You have been cycling for 5 days so far.
+# We've started the program for you.
+
+# 1.
+# The days variable is set to 0. It needs the correct value. Type in how many days you've been cycling so far.
+
+# 2.
+# Set the km_left variable to calculate:
+
+# total kilometres − (kilometres per day × days)
+
+# 3.
+# The print statement prints the km_left variable. Click run to see if it prints the correct answer:
+
+# 650
+# 4.
+# Don't forget to mark your program!
+
+
+
+
+total_km = 1000
+km_per_day = 70
+days = 0
+km_left = total_km - (km_per_day )
+
+print(km_left)
