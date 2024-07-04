2b0cb9693532181a2a0fecb96f793b28b8bc3c33
karanchawla1108 <karanchawla1108@gmail.com>
2024-07-04 19:30:17 +0100

    turtle

---
+++ turtle triangle.py
@@ -0,0 +1,40 @@
+# Let's draw a triangle!
+
+# We've given you the sizes for the side length and the angle, and have started the triangle. You need to complete it!
+
+# It should look like this:
+
+  
+# 1.
+# Click run to see what the program draws so far.
+
+# The turtle moves forward the number of steps stored in side, then turns left using angle for the degrees.
+
+# 2.
+# Move forward again to draw the next side. Use the variable side for the number of steps.
+
+# Then turn left again, using the angle variable.
+
+# 3.
+# Keep going! The rest of the code is up to you.
+
+# Run your code often to see if the turtle is on track.
+
+# 4.
+# Don't forget to mark your program!
+
+
+
+from turtle import *
+
+side = 100
+angle = 360 / 3
+
+forward(side)
+left(angle)
+forward(side)
+left (120)
+forward (side)
+
+
+# Add code above this line to finish the triangle!
