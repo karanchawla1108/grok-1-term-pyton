24fcfc145ab1e578b9a8c225c13c8795a5e24d3e
karanchawla1108 <karanchawla1108@gmail.com>
2024-07-08 14:30:25 +0100

    halve this

---
+++ Halve this.py
@@ -0,0 +1,17 @@
+# Write a program that reads in a whole number and prints out that number divided by two. Your program should work like this:
+
+# Number: 4
+# Half number: 2.0
+# ​
+# Or like this:
+
+# Number: 5
+# Half number: 2.5
+# ​
+
+
+
+number = float(input ("Number: "))
+
+print ("Half number:", number / 2)
+
